01335412430a957d8ff38394b66596e335d633c1
ddrieck <ddrieck@gmail.com>
2014-04-27 14:10:08 -0400

    Fixed bug introduced by adding trigger spans.

--- js/faqscript.js
+++ js/faqscript.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 		$('.faq-trigger').on("click", function(){
-			$(this).nextUntil('.glyphicon').slideToggle();
+			$(this).nextUntil('.faq-trigger').slideToggle();
 			$('.faq-section').stop();
 			var glyphSelect = $(this).find('.glyphicon');
 			if (glyphSelect.hasClass('glyphicon-plus')){
